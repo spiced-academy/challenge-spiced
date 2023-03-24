e06b769882ba0139dab4582a8af65865d42ac0dd
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-03-24 11:55:01 +0100

    not finished yet

--- javascript/toast-messages/js-createelement_toast-messages/js/index.js
+++ javascript/toast-messages/js-createelement_toast-messages/js/index.js
@@ -5,11 +5,22 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
-  toastContainer.innerHTML = "hello";
+  const toast = document.createElement("li");
+  toast.classList.add("toast - container__message");
+  toast.innerText = "something";
+  toastContainer.append(toast);
+
+  const listElement = document.createElement("li");
+
+  toastContainer.append(listElement);
+
+  listElement.classList.add("toast-container__message");
+  listElement.textContent = "Im a new message";
+  // toastContainer.innerHTML = "hello";
   // Exercise: Append a new entry to the toast messages container
 });
 
 clearButton.addEventListener("click", () => {
-  toastContainer.innerHTML = "";
+  // toastContainer.innerHTML = "";
   // Exercise: Clear the stack of toast messages
 });
