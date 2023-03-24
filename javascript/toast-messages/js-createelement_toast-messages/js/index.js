874777fe700f900ccbb91e800a751530e7d22ba3
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-03-24 10:59:16 +0100

    finished first challenge

--- javascript/toast-messages/js-createelement_toast-messages/js/index.js
+++ javascript/toast-messages/js-createelement_toast-messages/js/index.js
@@ -5,9 +5,11 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
+  toastContainer.innerHTML = "hello";
   // Exercise: Append a new entry to the toast messages container
 });
 
 clearButton.addEventListener("click", () => {
+  toastContainer.innerHTML = "";
   // Exercise: Clear the stack of toast messages
 });
